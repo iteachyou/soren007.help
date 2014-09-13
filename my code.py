c0327f376198298ace9baefb5f5daafee838fbad
soren0077 <goobinator007@gmail.com>
2014-09-13 16:54:19 -0400

    second code

--- my code.py
+++ my code.py
@@ -1,2 +1,15 @@
-name=input("what is your name?").strip().title()
-print("nice to meet you, " + name)
+name=input (" What is your name?").strip().title()
+if name == ("Soren"):
+    print("CopyCat!")
+else:
+    print ("nice to meet you " + name)
+
+
+di=input("Do you want to go right or left?").strip().lower()
+if di == ("right"):
+    print ("You deside to go right")
+
+if di == ("left"):
+    print ("You deside to go left")
+
+    
